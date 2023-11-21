c8c510e79bc13c9a5f60222f9ab4cf0544e7c9b8
Tony <lifeyun24@naver.com>
2023-11-21 09:28:35 +0900

    pdf file name change

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/TaewoongYun_Portfolio.pdf";
+import pdf from "../../Assets/YunTaeWoongPortfolio.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/tony-yun/Portfolio_myWebsiteCode/master/src/Assets/TaewoongYun_Portfolio.pdf";
+  "https://raw.githubusercontent.com/tony-yun/Portfolio_myWebsiteCode/master/src/Assets/YunTaeWoongPortfolio.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
